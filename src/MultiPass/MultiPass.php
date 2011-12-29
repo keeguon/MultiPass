06309c715f76b1877e74799b8d499dcf297d7b4c
Félix Bellanger <felix.bellanger@gmail.com>
2011-12-29 16:05:31 +0100

    Strategy instanciation should now work

--- src/MultiPass/MultiPass.php
+++ src/MultiPass/MultiPass.php
@@ -17,7 +17,7 @@ class MultiPass
     unset($opts['client_id'], $opts['client_secret']);
 
     // Instanciate provider
-    $strategy       = "\MultiPass\Strategy\{$provider}";
+    $strategy       = "\MultiPass\Strategy\\$provider";
     $this->provider = new $strategy($client_id, $client_secret, $opts);
   }
 
